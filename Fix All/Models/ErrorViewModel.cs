58c73bf8a4a1fd9df6f19ba0332dd7b2ae869700
abdullah <abdullah2309a@aptechorangi.com>
2025-07-23 19:48:01 -0400

    Add project files.

---
+++ Fix All/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fix_All.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
