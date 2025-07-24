e6a7f1b09514e24f8e2002e91a0c429ffc5636c0
abdullah <abdullah2309a@aptechorangi.com>
2025-07-24 00:09:21 -0400

    add Services page

--- Fix All/Controllers/HomeController.cs
+++ Fix All/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace Fix_All.Controllers
         {
             return View();
         }
-
+        public IActionResult Services()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
