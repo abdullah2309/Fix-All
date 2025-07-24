4af363641060e55f0c5343695288b6ea63b5d219
abdullah <abdullah2309a@aptechorangi.com>
2025-07-24 00:12:58 -0400

    add Contact page

--- Fix All/Controllers/HomeController.cs
+++ Fix All/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace Fix_All.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
