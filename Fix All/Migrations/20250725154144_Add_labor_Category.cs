76b3c45f4c1e25dee4e1f06814c653d65688d09d
abdullah <abdullah2309a@aptechorangi.com>
2025-07-25 11:42:15 -0400

    add  Migrations for Add_labor_Category

---
+++ Fix All/Migrations/20250725154144_Add_labor_Category.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fix_All.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_labor_Category : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "add_Labor_Categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    category_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_add_Labor_Categories", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "add_Labor_Categories");
+        }
+    }
+}
