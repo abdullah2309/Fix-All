b0f9555bd533f7443198571098a088bd044513a2
abdullah <abdullah2309a@aptechorangi.com>
2025-07-25 13:08:38 -0400

    edit admin controller  Add_Labor_Category

--- Fix All/Controllers/AdminController.cs
+++ Fix All/Controllers/AdminController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fix_All.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fix_All.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly mydbcontext mydbcontext;
+
+        public AdminController(mydbcontext mydbcontext)
+        {
+            this.mydbcontext = mydbcontext;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,6 +20,18 @@ namespace Fix_All.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Add_Labor_Category(Add_labor_Category add_Labor_Category)
+        {
+            if (ModelState.IsValid)
+            {
+                mydbcontext.add_Labor_Categories.Add(add_Labor_Category);
+                mydbcontext.SaveChanges();
+                return RedirectToAction("Add_Labor_Category");
+            }
+            return RedirectToAction(nameof(Add_Labor_Category));
+        }
+
         public IActionResult Table()
         {
             return View();
