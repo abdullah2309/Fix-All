4c7e12edb535dc40fc4dbe11d12b8c5468454dba
abdullah <abdullah2309a@aptechorangi.com>
2025-07-25 11:45:24 -0400

    edit ConnectionStrings

--- Fix All/Program.cs
+++ Fix All/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<mydbcontext>(a=>a.UseSqlServer("Server=DESKTOP-KHBGNKV\\SQLEXPRESS;Database=Fix_All; Trusted_Connection=True; TrustSeverCertificate=True"));
+builder.Services.AddDbContext<mydbcontext>(a=>a.UseSqlServer("Server=DESKTOP-KHBGNKV\\SQLEXPRESS;Database=Fix_All; Trusted_Connection=True; TrustServerCertificate=True"));
 
 var app = builder.Build();
 
