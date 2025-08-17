ba05d00fecf9b416511b16a0d5fb5ac6c1def88b
abdullah <abdullah2309a@aptechorangi.com>
2025-08-16 20:10:36 -0400

    add class ServiceProvider

---
+++ Fix All/Models/ServiceProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ServiceProvider
+{
+    [Key]
+    public int LarberId { get; set; } // Primary Key
+
+    [Required, MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required, MaxLength(50)]
+    public string LastName { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Phone { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; } // Store hashed password
+
+    [Required]
+    public string CNIC { get; set; }
+
+    [Required]
+    public string Address { get; set; }
+
+    [Required]
+    public string Education { get; set; }
+
+    public bool IsDiploma { get; set; }
+
+    [Required]
+    public int FieldId { get; set; } // Foreign Key
+
+    [ForeignKey("FieldId")]
+    public LaborField LaborField { get; set; }
+
+    public string CVFilePath { get; set; }
+    public string ProfileImagePath { get; set; }
+
+    public bool EmailVerified { get; set; }
+}
