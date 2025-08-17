5bd3ba9b441e7e295649aa2f23a24f9c12f8721b
abdullah <abdullah2309a@aptechorangi.com>
2025-08-16 22:35:50 -0400

    8:10 pm

--- Fix All/Controllers/HomeController.cs
+++ Fix All/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Fix_All.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Fix_All.Controllers
@@ -7,10 +9,12 @@ namespace Fix_All.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly mydbcontext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, mydbcontext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -22,22 +26,67 @@ namespace Fix_All.Controllers
         {
             return View();
         }
+
         public IActionResult Services()
         {
             return View();
         }
+
         public IActionResult Contact()
         {
-            return View(); 
+            return View();
         }
+
         public IActionResult Signin()
         {
             return View();
         }
+
         public IActionResult LaborSignin()
         {
+            ViewBag.Fields = new SelectList(_context.LaborFields, "FieldId", "Name");
             return View();
         }
+
+        //public IActionResult Register()
+        //{
+        //    ViewBag.Fields = new SelectList(_context.LaborFields, "FieldId", "Name");
+        //    return View();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Register(ServiceProvider model, IFormFile CVFile, IFormFile ProfileImage)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Fields = new SelectList(_context.LaborFields, "FieldId", "Name");
+                return View(model);
+            }
+
+            // Save files
+            if (CVFile != null && CVFile.Length > 0)
+            {
+                var filePath = Path.Combine("wwwroot/uploads/cv", CVFile.FileName);
+                using var stream = new FileStream(filePath, FileMode.Create);
+                await CVFile.CopyToAsync(stream);
+                model.CVFilePath = "/uploads/cv/" + CVFile.FileName;
+            }
+
+            if (ProfileImage != null && ProfileImage.Length > 0)
+            {
+                var imgPath = Path.Combine("wwwroot/uploads/images", ProfileImage.FileName);
+                using var stream = new FileStream(imgPath, FileMode.Create);
+                await ProfileImage.CopyToAsync(stream);
+                model.ProfileImagePath = "/uploads/images/" + ProfileImage.FileName;
+            }
+
+            _context.ServiceProviders.Add(model);
+            await _context.SaveChangesAsync();
+
+            TempData["Success"] = "Application submitted successfully!";
+            return RedirectToAction("LaborSignin");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
