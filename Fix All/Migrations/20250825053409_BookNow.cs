4e8864791605c89220b4994844409e1a1eaca0f8
abdullah <abdullah2309a@aptechorangi.com>
2025-08-25 01:34:49 -0400

    add-migration booknow

---
+++ Fix All/Migrations/20250825053409_BookNow.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fix_All.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookNow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BookingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ApproveLarberId = table.Column<int>(type: "int", nullable: false),
+                    ApproveLaberApproveLarberId = table.Column<int>(type: "int", nullable: false),
+                    FieldId = table.Column<int>(type: "int", nullable: false),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ServiceAddress = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BookingId);
+                    table.ForeignKey(
+                        name: "FK_Bookings_LaborFields_FieldId",
+                        column: x => x.FieldId,
+                        principalTable: "LaborFields",
+                        principalColumn: "FieldId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bookings_UserAccounts_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bookings_approve_labers_ApproveLaberApproveLarberId",
+                        column: x => x.ApproveLaberApproveLarberId,
+                        principalTable: "approve_labers",
+                        principalColumn: "ApproveLarberId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_ApproveLaberApproveLarberId",
+                table: "Bookings",
+                column: "ApproveLaberApproveLarberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_FieldId",
+                table: "Bookings",
+                column: "FieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
