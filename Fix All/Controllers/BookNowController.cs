fea9a5c98d90625581cf1a2086c298fce2e39a02
abdullah <abdullah2309a@aptechorangi.com>
2025-08-25 01:49:59 -0400

    add BookNowController

---
+++ Fix All/Controllers/BookNowController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fix_All.Controllers
+{
+    public class BookNowController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
