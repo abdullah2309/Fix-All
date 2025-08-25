84a66c127a017f64681220c5745d95fd2a4d9222
abdullah <abdullah2309a@aptechorangi.com>
2025-08-25 00:20:50 -0400

    add-migration UserAccounts

--- Fix All/Migrations/20250825042036_UserAccounts.Designer.cs
+++ Fix All/Migrations/20250825042036_UserAccounts.Designer.cs
@@ -4,6 +4,7 @@ using Fix_All.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fix_All.Migrations
 {
     [DbContext(typeof(mydbcontext))]
-    partial class mydbcontextModelSnapshot : ModelSnapshot
+    [Migration("20250825042036_UserAccounts")]
+    partial class UserAccounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
