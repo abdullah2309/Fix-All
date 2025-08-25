c340e05a0d76d187ea107a9d0f54c9d4d8cad917
abdullah <abdullah2309a@aptechorangi.com>
2025-08-25 16:15:18 -0400

    update AdminLogin

--- Fix All/Models/AdminLogin.cs
+++ Fix All/Models/AdminLogin.cs
@@ -1,6 +1,21 @@
-﻿namespace Fix_All.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YourProject.Models
 {
     public class AdminLogin
     {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Admin Email")]
+        public string Email { get; set; } = "admin@email.com";
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = "adminAbdullah123";
     }
 }
