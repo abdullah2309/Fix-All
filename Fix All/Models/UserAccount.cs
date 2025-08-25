e2ceaf6078de6efc320b456838b4f7f5d17dad0a
abdullah <abdullah2309a@aptechorangi.com>
2025-08-25 00:17:33 -0400

    add UserAccount.cs

---
+++ Fix All/Models/UserAccount.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fix_All.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Contact Number is required")]
+        [Phone]
+        public string ContactNumber { get; set; }
+
+        // Extra fields if needed
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
