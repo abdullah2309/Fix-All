32766240ba9fe0ef881d43c4b4fcacfa9a2c6d24
abdullah <abdullah2309a@aptechorangi.com>
2025-08-27 00:24:28 -0400

    add feedback.cs

---
+++ Fix All/Models/Feedback.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fix_All.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int FeedbackId { get; set; }
+
+        [Required]
+        [ForeignKey("BookNow")]
+        public int BookingId { get; set; }
+        public BookNow BookNow { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, EmailAddress, StringLength(150)]
+        public string Email { get; set; }
+
+        [Required, Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required, StringLength(1000)]
+        public string Message { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
