c4470d8aef35b0642327dfe33b4560c23b865288
abdullah <abdullah2309a@aptechorangi.com>
2025-08-27 00:29:54 -0400

    add Create.cshtml

--- Fix All/Controllers/FeedbackController.cs
+++ Fix All/Controllers/FeedbackController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fix_All.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fix_All.Controllers
 {
     public class FeedbackController : Controller
     {
-        public IActionResult Index()
+        private readonly mydbcontext _context;
+
+        public FeedbackController(mydbcontext context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create(int bookingId)
+        {
+            ViewBag.BookingId = bookingId;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Feedbacks.Add(feedback);
+                _context.SaveChanges();
+                TempData["Success"] = "Thanks for your feedback!";
+                return RedirectToAction("MyBookings", "UserPanel"); // redirect to customer panel
+            }
+
+            return View(feedback);
         }
     }
 }
