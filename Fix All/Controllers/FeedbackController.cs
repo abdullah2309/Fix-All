5f6c00087f26fe6ff6025e837f076fb0fc76a744
abdullah <abdullah2309a@aptechorangi.com>
2025-08-27 00:28:20 -0400

    add FeedbackController.cs

---
+++ Fix All/Controllers/FeedbackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fix_All.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
